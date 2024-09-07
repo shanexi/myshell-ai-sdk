ae8805d909ad64404147c68d1dcfa3e6aed7de1c
shanexi <shane@myshell.ai>
2024-09-07 15:19:12 +0800

    build: config storybook-react-rspack

--- libs/chat/.storybook/main.ts
+++ libs/chat/.storybook/main.ts
@@ -1,15 +1,44 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
-const config: StorybookConfig = {
-  framework: '@storybook/react-webpack5',
-  stories: [
-    '../src/lib/**/*.stories.mdx',
-    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: ['@storybook/addon-essentials', '@nrwl/react/plugins/storybook'],
-};
+function makeConfig(useRspack = true): StorybookConfig {
+  const framework = useRspack
+    ? 'storybook-react-rspack'
+    : '@storybook/react-webpack5';
+  return {
+    core: {
+      disableTelemetry: true,
+    },
+    framework: {
+      name: '@storybook/react-webpack5',
+      options: Object.assign(
+        {
+          fastRefresh: true,
+          strictMode: true,
+          legacyRootApi: true,
+        },
+        useRspack
+          ? {}
+          : {
+              useSWC: true,
+              lazyCompilation: true,
+              fsCache: true,
+            }
+      ),
+    },
+    stories: [
+      '../src/lib/**/*.stories.mdx',
+      '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: ['@storybook/addon-essentials', '@nrwl/react/plugins/storybook'],
+    // https://storybook.js.org/docs/api/main-config-typescript#skipbabel
+    typescript: {
+      check: false,
+      reactDocgen: false,
+    },
+  };
+}
 
-module.exports = config;
+module.exports = makeConfig();
 
 // To customize your webpack configuration you can use the webpackFinal field.
 // Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
---
+++ libs/chat/.storybook/rspack.config.js
@@ -0,0 +1,10 @@
+/* https://github.com/rspack-contrib/rspack-examples/blob/main/rspack/react-storybook/.storybook/rspack.config.js */
+const path = require('path');
+module.exports = ({ config }) => {
+  // eslint-disable-next-line no-param-reassign
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    '@myshell-ai-sdk/def': path.resolve(__dirname, '../../def/src/index.ts'),
+  };
+  return config;
+};
