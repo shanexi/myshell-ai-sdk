41919b8de4edf007d8a33c6a1c5c87d04f0e79a6
shanexi <shane@myshell.ai>
2024-09-08 22:47:22 +0800

    test: zustand immer

--- libs/chat/src/lib/zustand-simple.slice.spec.ts
+++ libs/chat/src/lib/zustand-simple.slice.spec.ts
@@ -6,75 +6,75 @@ import {
   FishSlice,
   SharedSlice,
 } from './zustand-simple.slice';
-import { create } from 'zustand';
+import { createStore } from 'zustand/vanilla';
 
 describe('chat slice', () => {
   it('should initialize with 0 fishes', () => {
-    const useStore = create(createFishSlice);
-    const state = useStore.getState();
+    const store = createStore(createFishSlice);
+    const state = store.getState();
     expect(state.fishes).toBe(0);
   });
 
   it('should add a fish', () => {
-    const useStore = create(createFishSlice);
-    const state = useStore.getState();
+    const store = createStore(createFishSlice);
+    const state = store.getState();
     state.addFish();
-    expect(useStore.getState().fishes).toBe(1);
+    expect(store.getState().fishes).toBe(1);
   });
 
   it('should initialize with 0 bears', () => {
-    const useStore = create<BearSlice & FishSlice>((...a) => ({
+    const store = createStore<BearSlice & FishSlice>((...a) => ({
       ...createBearSlice(...a),
       ...createFishSlice(...a),
     }));
-    const state = useStore.getState();
+    const state = store.getState();
     expect(state.bears).toBe(0);
   });
 
   it('should add a bear', () => {
-    const useStore = create<BearSlice & FishSlice>((...a) => ({
+    const store = createStore<BearSlice & FishSlice>((...a) => ({
       ...createBearSlice(...a),
       ...createFishSlice(...a),
     }));
-    const state = useStore.getState();
+    const state = store.getState();
     state.addBear();
-    expect(useStore.getState().bears).toBe(1);
+    expect(store.getState().bears).toBe(1);
   });
 
   it('should eat a fish', () => {
-    const useStore = create<BearSlice & FishSlice>((...a) => ({
+    const store = createStore<BearSlice & FishSlice>((...a) => ({
       ...createBearSlice(...a),
       ...createFishSlice(...a),
     }));
-    const state = useStore.getState();
+    const state = store.getState();
     state.addFish(); // Ensure there is at least one fish to eat
     state.eatFish();
-    expect(useStore.getState().fishes).toBe(0);
+    expect(store.getState().fishes).toBe(0);
   });
 });
 
 describe('createSharedSlice', () => {
   it('should add both a bear and a fish', () => {
-    const useStore = create<BearSlice & FishSlice & SharedSlice>((...a) => ({
+    const store = createStore<BearSlice & FishSlice & SharedSlice>((...a) => ({
       ...createBearSlice(...a),
       ...createFishSlice(...a),
       ...createSharedSlice(...a),
     }));
 
-    const state = useStore.getState();
+    const state = store.getState();
     state.addBoth();
-    expect(useStore.getState().bears).toBe(1);
-    expect(useStore.getState().fishes).toBe(1);
+    expect(store.getState().bears).toBe(1);
+    expect(store.getState().fishes).toBe(1);
   });
 
   it('should get the total number of bears and fishes', () => {
-    const useStore = create<BearSlice & FishSlice & SharedSlice>((...a) => ({
+    const store = createStore<BearSlice & FishSlice & SharedSlice>((...a) => ({
       ...createBearSlice(...a),
       ...createFishSlice(...a),
       ...createSharedSlice(...a),
     }));
 
-    const state = useStore.getState();
+    const state = store.getState();
     state.addBear();
     state.addFish();
     expect(state.getBoth()).toBe(2); // 1 bear + 1 fish
