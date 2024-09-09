aa270083705bf163b88a4c9412508462fcc6e2a6
shanexi <shane@myshell.ai>
2024-09-09 14:45:47 +0800

    chore: immer with auto selectors

---
+++ libs/chat/src/lib/zustand-immer-auto.tsx
@@ -0,0 +1,68 @@
+/** @jsxImportSource zustand-signal */
+
+import { create } from 'zustand';
+import {
+  BearSlice,
+  createBearSlice,
+  createFishSlice,
+  createSharedSlice,
+  FishSlice,
+  SharedSlice,
+} from './zustand-immer.slice';
+import { immer } from 'zustand/middleware/immer';
+import { computed } from 'zustand-computed-state';
+import { createSelectorFunctions } from 'auto-zustand-selectors-hook';
+
+export const useBoundStoreBase = create<BearSlice & FishSlice & SharedSlice>()(
+  computed(
+    immer((...a) => ({
+      ...createBearSlice(...a),
+      ...createFishSlice(...a),
+      ...createSharedSlice(...a),
+    }))
+  )
+);
+
+const useBoundStore = createSelectorFunctions(useBoundStoreBase);
+
+function BearCounter() {
+  const { bears, fishes, ySq } = useBoundStore.use;
+  return (
+    <div>
+      <div>{bears()} bears around here...</div>
+      <div>{fishes()} fishes around here...</div>
+      <div>{ySq()} ySq around here...</div>
+    </div>
+  );
+}
+
+function AddBear() {
+  return <button onClick={useBoundStore.use.addBear()}>add bear</button>;
+}
+
+function AddFish() {
+  return <button onClick={useBoundStore.use.addFish()}>add fish</button>;
+}
+
+function AddBoth() {
+  return <button onClick={useBoundStore.use.addBoth()}>add both</button>;
+}
+
+function EatFish() {
+  return <button onClick={useBoundStore.use.eatFish()}>eat fish</button>;
+}
+
+export function ImmerAuto() {
+  return (
+    <div>
+      <h1>Zustand immer auto selectors!</h1>
+      <BearCounter />
+      <AddBear />
+      <EatFish />
+      <AddFish />
+      <AddBoth />
+    </div>
+  );
+}
+
+export default ImmerAuto;
