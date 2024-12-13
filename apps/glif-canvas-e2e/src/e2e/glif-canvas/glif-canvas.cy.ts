b16d086b2e45422a32baf5a80a61e2b538a61ade
shanexi <shane@myshell.ai>
2024-12-13 18:56:56 +0800

    remove glif

--- apps/glif-canvas-e2e/src/e2e/glif-canvas/glif-canvas.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('glif-canvas: GlifCanvas component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=glifcanvas--primary'));
-
-  it('should render the component', () => {
-    cy.get('h1').should('contain', 'Welcome to GlifCanvas!');
-  });
-});
