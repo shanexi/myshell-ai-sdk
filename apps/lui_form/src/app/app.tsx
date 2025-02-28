45849ecbda72852d2ea18f8085ab76167cfbfeba
shanexi <shane@myshell.ai>
2025-02-28 16:18:33 +0800

    chore: add lui_form

---
+++ apps/lui_form/src/app/app.tsx
@@ -0,0 +1,14 @@
+export function App() {
+  return (
+    <>
+      <h1>
+        <span> Hello there, </span>
+        Welcome lui-form 👋
+      </h1>
+
+      <div />
+    </>
+  );
+}
+
+export default App;
