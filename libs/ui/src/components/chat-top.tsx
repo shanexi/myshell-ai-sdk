3c62e4c9b4779b0f6c07397df76990a34880a622
shanexi <shane@myshell.ai>
2025-03-03 18:16:22 +0800

    chore: chat top

---
+++ libs/ui/src/components/chat-top.tsx
@@ -0,0 +1,13 @@
+import { ReactComponent as ArrowLeft } from './arrow-left.svg';
+import { ReactComponent as Ellipsis } from './ellipsis.svg';
+import { ChatTopTab } from './chat-top-tab';
+
+export function ChatTop() {
+  return (
+    <div className="flex items-center justify-between">
+      <ArrowLeft />
+      <ChatTopTab />
+      <Ellipsis />
+    </div>
+  );
+}
