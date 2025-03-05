5282eb395917d73ccc9f8fce35054f785927c39a
shanexi <shane@myshell.ai>
2025-03-05 10:07:48 +0800

    refactor: split own and reply message item

--- libs/ui/src/components/message-item.tsx
+++ libs/ui/src/components/message-item.tsx
@@ -1,5 +1,4 @@
 import { type VirtuosoMessageListProps } from '@virtuoso.dev/message-list';
-import { cn } from '../utils';
 
 export interface Message {
   key: string;
@@ -10,26 +9,35 @@ export interface Message {
 export const MessageItem: VirtuosoMessageListProps<
   Message,
   null
->['ItemContent'] = ({ data }) => {
+>['ItemContent'] = (props) => {
+  const { data } = props;
   const ownMessage = data.user === 'me';
+  if (ownMessage) return <OwnMessageItem {...data} />;
+  return <ReplyMessageItem {...data} />;
+};
 
+export const OwnMessageItem = (props: Message) => {
   return (
     <div className="flex pb-8">
-      {ownMessage ? null : (
-        <img
-          className="mr-[8px] h-[32px] w-[32px] rounded-lg"
-          src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-        />
-      )}
-      <div
-        className={cn(
-          'max-w-[80%] rounded-br-[16px] rounded-bl-[16px] p-4',
-          ownMessage
-            ? 'ml-auto rounded-tl-[16px] rounded-tr-[2px] bg-surface-primary-subtle-default-light text-text-brand-light'
-            : 'rounded-tl-[2px] rounded-tr-[16px] bg-surface-default-light text-text-default-light',
-        )}
-      >
-        {data.text}
+      <div className="ml-auto max-w-[80%] rounded-tl-[16px] rounded-tr-[2px] rounded-br-[16px] rounded-bl-[16px] bg-surface-primary-subtle-default-light p-4 text-text-brand-light">
+        {props.text}
+      </div>
+    </div>
+  );
+};
+
+export const ReplyMessageItem = (props: Message) => {
+  return (
+    <div className="flex pb-8">
+      <img
+        className="mr-[8px] h-[32px] w-[32px] rounded-lg"
+        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+      />
+      <div className="max-w-[80%]">
+        <div className="rounded-tl-[2px] rounded-tr-[16px] rounded-br-[16px] rounded-bl-[16px] bg-surface-default-light p-4 text-text-default-light">
+          {props.text}
+        </div>
+        <div className="mt-[8px]">button</div>
       </div>
     </div>
   );
