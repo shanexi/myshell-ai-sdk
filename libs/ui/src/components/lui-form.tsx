6edd2aa5ac8015d1966e1ce466f05f1c45d827e7
shanexi <shane@myshell.ai>
2025-03-06 09:45:25 +0800

    chore: upload

--- libs/ui/src/components/lui-form.tsx
+++ libs/ui/src/components/lui-form.tsx
@@ -1,11 +1,28 @@
 import { LuiFormButton } from './lui-form-button';
+import { ReactComponent as ArrowUpTray } from './arrow-up-tray.svg';
 
 export const LuiForm = () => {
   return (
     <div>
       <LuiFormHeader />
       <div className="px-spacing-xl pt-spacing-lg pb-spacing-3xl">
-        Form body
+        <div className="text-sm-medium mb-[4px]">Title</div>
+        <div className="text-sm-regular mb-[6px]">Description</div>
+        <div className="rounded-lg border border-border-default-light bg-surface-default-light p-spacing-xl shadow-background-default">
+          <div className="flex items-center">
+            <div className="mr-spacing-lg rounded-[10px] bg-surface-accent-gray-subtlest-light p-[12px]">
+              <ArrowUpTray />
+            </div>
+            <div>
+              <div className="text-sm-medium text-text-default-light">
+                Drop a file or click to upload
+              </div>
+              <div className="description-lg-regular text-text-subtler-light">
+                PNG, JPG, GIF up to 10MB
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <LuiFormFooter />
     </div>
