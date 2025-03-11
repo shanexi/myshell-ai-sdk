affc3ea5ddc9a4b7668ed13581687c03d3b932dd
shanexi <shane@myshell.ai>
2025-03-11 23:12:24 +0800

    chore: chat input menu

--- libs/ui/src/components/chat-input.tsx
+++ libs/ui/src/components/chat-input.tsx
@@ -1,11 +1,33 @@
 import { AlignJustify, CirclePlus } from 'lucide-react';
 import { ReactComponent as Audio } from './audio.svg';
+import { ReactComponent as Clear } from './clear.svg';
+import { ReactComponent as Trash } from './trash.svg';
+import { MenuTrigger } from 'react-aria-components';
+import {
+  Button,
+  Menu,
+  MenuItem,
+} from '@myshell-run/react-aria-tailwind-starter';
 
 export function ChatInput() {
   return (
     <div className="mx-[8px] my-spacing-md">
       <label className="input w-full rounded-4xl border-border-default-light focus-within:ring-0 focus-within:outline-none focus:ring-0 focus:outline-none">
-        <AlignJustify className="text-text-brand-light" />
+        <MenuTrigger>
+          <Button variant="icon">
+            <AlignJustify className="text-text-brand-light" />
+          </Button>
+          <Menu>
+            <MenuItem id="clear">
+              <Clear className="h-5 w-5" />
+              Clear Memory
+            </MenuItem>
+            <MenuItem id="delete">
+              <Trash className="h-5 w-5" />
+              Delete Chat History
+            </MenuItem>
+          </Menu>
+        </MenuTrigger>
         <CirclePlus className="text-text-brand-light" />
         <input type="search" className="grow" placeholder="Write a message" />
         <Audio />
