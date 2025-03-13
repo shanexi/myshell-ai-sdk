1232693c9675da6d0338cd59c4a23776cf7f34e7
shanexi <shane@myshell.ai>
2025-03-13 15:49:26 +0800

    refactor: restrict some basic components as frame role to a seperate project

--- libs/message-item-plugins/src/components/reply-message.tsx
+++ libs/message-item-plugins/src/components/reply-message.tsx
@@ -4,27 +4,7 @@ import { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { useInjection } from 'inversify-react';
 import { ExecutingMessageModel } from './executing-message-model';
-
-export const ReplyMessageFrame = (props: {
-  children: React.ReactNode;
-  button?: React.ReactNode;
-}) => {
-  const { children, button } = props;
-  return (
-    <div className="flex pb-8">
-      <img
-        className="mr-[8px] h-[32px] w-[32px] rounded-lg"
-        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-      />
-      <div className="w-[80%]">
-        <div className="rounded-tl-[2px] rounded-tr-[16px] rounded-br-[16px] rounded-bl-[16px] bg-surface-default-light p-4 text-text-default-light">
-          {children}
-        </div>
-        {button && <div className="mt-[8px]">{button}</div>}
-      </div>
-    </div>
-  );
-};
+import { ReplyMessageFrame } from '@myshell-run/ui-primitives';
 
 // todo 增加一些动效 当从后端收到真实倒计时的时候（重置）做一个闪烁的效果 然后再更新值
 export const ExecutingMessage = observer((props: Message) => {
