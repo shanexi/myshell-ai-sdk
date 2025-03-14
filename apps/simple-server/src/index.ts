9cf51492f13d75400498be28eaa17478773e808e
shanexi <shane@myshell.ai>
2025-03-14 23:40:09 +0800

    chore: hono + trpc

--- apps/simple-server/src/index.ts
+++ apps/simple-server/src/index.ts
@@ -1,6 +1,15 @@
 import { Hono } from 'hono';
+import { trpcServer } from '@hono/trpc-server';
+import { appRouter } from './app-router';
 
 const app = new Hono();
+
+app.use(
+  '/trpc/*',
+  trpcServer({
+    router: appRouter,
+  }),
+);
 
 app.get('/', (c) => c.text('Hono!'));
 
