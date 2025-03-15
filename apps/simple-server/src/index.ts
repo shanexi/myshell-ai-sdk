f18ba0481ce2915190935f4d0419ef3345839af9
shanexi <shane@myshell.ai>
2025-03-15 09:20:15 +0800

    refactor: inject and get prisma client from ioc

--- apps/simple-server/src/index.ts
+++ apps/simple-server/src/index.ts
@@ -2,6 +2,15 @@ import { Hono } from 'hono';
 import { trpcServer } from '@hono/trpc-server';
 import { appRouter } from './app-router';
 import prismaClients from './prisma';
+import { Container } from 'inversify';
+import { loadModule, PrismaClientProvider } from './loadModule';
+
+const container = new Container();
+loadModule(container);
+
+const prismaClientProvider = container.get<PrismaClientProvider>(
+  'PrismaClientProvider',
+);
 
 const app = new Hono<{ Bindings: Env }>();
 
@@ -13,8 +22,7 @@ app.use(
 );
 
 app.get('/', async (c) => {
-  console.log('c.env', c.env);
-  const prisma = await prismaClients.fetch(c.env.DB_MYSHELL_RUN_TEST);
+  const prisma = await prismaClientProvider(c.env.DB_MYSHELL_RUN_TEST);
   const users = await prisma.user.findMany();
   return c.json(users);
 });
--- apps/simple-server/src/prisma.ts
+++ apps/simple-server/src/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client-simple';
 import { PrismaD1 } from '@prisma/adapter-d1';
 
 const prismaClients = {
-  async fetch(db: D1Database) {
+  async fetch(db: D1Database): Promise<PrismaClient> {
     const adapter = new PrismaD1(db);
     const prisma = new PrismaClient({ adapter });
     return prisma;
