16883304837198d85b22319242fa6b892cf7f3e1
shanexi <shane@myshell.ai>
2025-03-15 08:39:26 +0800

    wip: hono + prisma

--- apps/simple-server/src/index.ts
+++ apps/simple-server/src/index.ts
@@ -1,8 +1,9 @@
 import { Hono } from 'hono';
 import { trpcServer } from '@hono/trpc-server';
 import { appRouter } from './app-router';
+import prismaClients from './prisma';
 
-const app = new Hono();
+const app = new Hono<{ Bindings: Env }>();
 
 app.use(
   '/trpc/*',
@@ -11,6 +12,12 @@ app.use(
   }),
 );
 
-app.get('/', (c) => c.text('Hono!'));
+app.get('/', async (c) => {
+  console.log('c.env', c.env);
+  const prisma = await prismaClients.fetch(c.env.DB_MYSHELL_RUN_TEST);
+  const users = await prisma.user.findMany();
+  console.log('users', users);
+  return c.json(users);
+});
 
 export default app;
