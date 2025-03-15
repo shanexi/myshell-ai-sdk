a6bae4d6f6f6be754bdf87844c13f832416c3e49
shanexi <shane@myshell.ai>
2025-03-15 19:43:31 +0800

    chore: #60 add a simple honox app

--- apps/my-app/vite.config.ts
+++ apps/my-app/vite.config.ts
@@ -2,15 +2,21 @@ import build from '@hono/vite-build/cloudflare-workers';
 import adapter from '@hono/vite-dev-server/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 import honox from 'honox/vite';
+import ssg from '@hono/vite-ssg';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [
-    honox({
-      devServer: { adapter },
-      client: { input: ['./app/style.css'] },
-    }),
-    tailwindcss(),
-    build(),
-  ],
+export default defineConfig(({ mode }) => {
+  // default
+  const entry = './app/server.ts';
+  return {
+    plugins: [
+      honox({
+        devServer: { adapter },
+        client: { input: ['./app/style.css'] },
+      }),
+      tailwindcss(),
+      // ssg({ entry }),
+      build(),
+    ],
+  };
 });
