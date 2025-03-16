c558cbb3417fed199396b902841616adc93e646e
shanexi <shane@myshell.ai>
2025-03-16 21:04:48 +0800

    feat: #60 kysely-d1 works

--- apps/my-app/app/loadModule.ts
+++ apps/my-app/app/loadModule.ts
@@ -1,5 +1,4 @@
 import { Container, interfaces } from 'inversify';
-import { type PrismaClient } from '@prisma/client-simple-pg/edge';
 import { TrpcRouter } from './trpc-router';
 import { isNotSSG } from './constants';
 
--- apps/my-app/app/routes/index.tsx
+++ apps/my-app/app/routes/index.tsx
@@ -2,6 +2,8 @@ import { createFactory } from 'hono/factory';
 
 import Counter from '../islands/counter';
 import type { Meta } from './types';
+import { Kysely } from 'kysely';
+import { D1Dialect } from '../kysely-d1';
 
 const factory = createFactory<{
   Bindings: Env;
@@ -15,12 +17,20 @@ export type User = {
   name: string;
 };
 
+interface Database {
+  user: User;
+}
+
 export default createRoute(async (c) => {
   let users: User[] = [];
   if (import.meta.env.VITE_SSG !== '1') {
-    const db = c.env.DB_MYSHELL_RUN_TEST;
-    const { results } = await db.prepare('SELECT * FROM User').all<User>();
-    users = results;
+    // const db = c.env.DB_MYSHELL_RUN_TEST;
+    // const { results } = await db.prepare('SELECT * FROM User').all<User>();
+    // users = results;
+    const db = new Kysely<Database>({
+      dialect: new D1Dialect({ database: c.env.DB_MYSHELL_RUN_TEST }),
+    });
+    users = await db.selectFrom('user').selectAll().execute();
   }
 
   const name = c.req.query('name') ?? 'Hono';
