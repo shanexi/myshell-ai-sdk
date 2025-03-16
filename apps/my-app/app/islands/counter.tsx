fd592936bc4a584f3892fb25216c8d7a68754814
shanexi <shane@myshell.ai>
2025-03-16 22:24:42 +0800

    feat: #63 react render (#64)

--- apps/my-app/app/islands/counter.tsx
+++ apps/my-app/app/islands/counter.tsx
@@ -1,16 +1,29 @@
-import { useState } from 'hono/jsx';
+import { observer } from 'mobx-react-lite';
+import { action, makeObservable, observable } from 'mobx';
+class CounterStore {
+  count = 0;
+  constructor() {
+    makeObservable(this, {
+      count: observable,
+      increment: action,
+    });
+  }
+  increment = () => {
+    this.count++;
+  };
+}
+const store = new CounterStore();
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
+export const Counter = observer(() => {
   return (
     <div>
-      <p class="py-2 text-2xl">{count}</p>
+      <p className="py-2 text-2xl">{store.count}</p>
       <button
-        class="px-4 py-2 bg-orange-400 text-white rounded cursor-pointer"
-        onClick={() => setCount(count + 1)}
+        className="cursor-pointer rounded bg-orange-400 px-4 py-2 text-white"
+        onClick={() => store.increment()}
       >
         Increment
       </button>
     </div>
   );
-}
+});
--- apps/my-app/app/routes/_404.tsx
+++ apps/my-app/app/routes/_404.tsx
@@ -2,7 +2,7 @@ import type { NotFoundHandler } from 'hono';
 
 const handler: NotFoundHandler = (c) => {
   c.status(404);
-  return c.render('404 Not Found');
+  return c.render(<>404 Not Found</>);
 };
 
 export default handler;
