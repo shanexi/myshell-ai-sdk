42d2f4c14a71dee8970c51a4433bfd3ac243fd8a
shanexi <shane@myshell.ai>
2025-03-17 22:34:14 +0800

    feat: vis bundle zie

--- apps/my-app/app/routes/_renderer.tsx
+++ apps/my-app/app/routes/_renderer.tsx
@@ -6,8 +6,10 @@ export default reactRenderer(({ children }) => {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {import.meta.env.PROD ? (
+        {import.meta.env.PROD || import.meta.env.VITE_SSG === '1' ? (
           <>
+            <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
+            <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
             <script type="module" src="/static/client.js"></script>
             <link href="/static/assets/style.css" rel="stylesheet" />
           </>
