baa4d3311ed8ab23257314d7ef3ce950a026d198
shanexi <shane@myshell.ai>
2025-03-20 15:41:17 +0800

    fix: chat

--- apps/chat/src/main.tsx
+++ apps/chat/src/main.tsx
@@ -3,13 +3,30 @@ import '@myshell-run/tailwind-cfg/styles.css';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
-import { Chat } from '@myshell-run/ui-biz';
+import {
+  BotInfo,
+  ChatFoot,
+  ChatInputRoot,
+  ChatMessageList,
+  ChatRoot,
+  ChatTopMenu,
+  ChatTopRoot,
+} from '@myshell-run/ui-biz';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <StrictMode>
-    <Chat />
+    <ChatRoot>
+      <ChatTopRoot>
+        <ChatTopMenu />
+      </ChatTopRoot>
+      <ChatMessageList className="flex flex-grow flex-col overflow-auto px-[8px]" />
+      <ChatFoot>
+        <ChatInputRoot />
+        <BotInfo />
+      </ChatFoot>
+    </ChatRoot>
   </StrictMode>,
 );
