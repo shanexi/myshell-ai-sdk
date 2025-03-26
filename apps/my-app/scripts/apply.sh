84463b8a27fafb942c5e53744c132e9fcb837249
shanexi <shane@myshell.ai>
2025-03-26 14:52:57 +0800

    build: apply.sh update

--- apps/my-app/scripts/apply.sh
+++ apps/my-app/scripts/apply.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-npx wrangler d1 migrations apply $(cat ./wrangler.jsonc | sed 's/^ *\/\/.*//' | jq -r '.d1_databases[0].database_name') --local
+deploy_env=${1:---local}
+output=$(npx wrangler d1 migrations apply $(cat ./wrangler.jsonc | sed 's/^ *\/\/.*//' | jq -r '.d1_databases[0].database_name') $deploy_env | tee /dev/tty)
 
-# prisma generate
-migrations_dir=$(cat wrangler.jsonc | sed 's/^ *\/\/.*//' | jq -r '.d1_databases[0].migrations_dir')
-prisma_dir=$(echo $migrations_dir | sed 's/\/prisma\/migrations//')
-cd $prisma_dir
-npm run generate
+output_msg=$(echo "$output" | tail -n 1)
+
+if [[ $output_msg == *"Migrations applied successfully"* ]]; then
+  # prisma generate
+  migrations_dir=$(cat wrangler.jsonc | sed 's/^ *\/\/.*//' | jq -r '.d1_databases[0].migrations_dir')
+  prisma_dir=$(echo $migrations_dir | sed 's/\/prisma\/migrations//')
+  cd $prisma_dir
+  npm run generate
+fi
