27edada217fcad2eed4b4546c08e85de3478a9b3
shanexi <shane@myshell.ai>
2025-03-26 11:12:44 +0800

    feat: add anti-corruption layer for biz

---
+++ libs/ac-biz/jest.config.ts
@@ -0,0 +1,18 @@
+/* eslint-disable */
+export default {
+  displayName: 'ac-biz',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/chatModel',
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+};
