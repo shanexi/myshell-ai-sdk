73f8744088d6532aa3be742e86e01ce0d38ed3ba
shanexi <shane@myshell.ai>
2025-03-26 15:39:59 +0800

    feat: 优化性能

--- apps/my-app/app/routes/bots.tsx
+++ apps/my-app/app/routes/bots.tsx
@@ -19,7 +19,16 @@ export default createRoute(async (c) => {
     const db = new Kysely<Database>({
       dialect: new D1Dialect({ database: c.env.DB_MYSHELL_RUN_TEST }),
     });
-    bots = await db.selectFrom('bot').selectAll().execute();
+    bots = await db
+      .selectFrom('bot')
+      .select(['id', 'avatar', 'name', 'isOfficial', 'description'])
+      .limit(30)
+      .execute();
+    bots = bots.map((bot) => ({
+      ...bot,
+      // FIXME 0 50 会导致 hyrdate 有 diff 奇怪 似乎是碰到了一些特殊字符
+      description: bot.description?.slice(0, 60),
+    }));
   }
   return c.render(
     <BotListRoot>
