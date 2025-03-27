260888762ca88002106dbda4f27c93e0a82c37c2
shanexi <shane@myshell.ai>
2025-03-27 21:50:51 +0800

    refactor: clean code

--- apps/my-app/app/islands/chat.tsx
+++ apps/my-app/app/islands/chat.tsx
@@ -19,13 +19,12 @@ export const ChatMessageListIsland = (props: {
   const { className, licenseKey, initialMessages, botAvatar } = props;
   return (
     <InversifyProvider container={clientContainer}>
-      {/* 必须 flex flex-col 才能让 `virtuoso.current.scrollToItem({ index: 0, align: "end" })` 正常工作，原因未细究，参考 https://virtuoso.dev/virtuoso-message-list/examples/ai-chatbot/ */}
-      {/* FIXME 这里和 ChatMessageList 的 flex flex-col 重复了 但是效果上没有问题 */}
       <ChatMessageList
         className={className}
         licenseKey={licenseKey}
         initialMessages={initialMessages.map((msg) => ({
           ...msg,
+          // 这样写 而不是在 ssr 层就 map 好，是为了减少 ssr 体积
           avatar: botAvatar,
         }))}
       />
--- apps/my-app/app/islands/client.container.ts
+++ apps/my-app/app/islands/client.container.ts
@@ -1,7 +1,6 @@
 import { messageItemPluginsModule } from '@myshell-run/message-item-plugins';
 import { uiBizModule } from '@myshell-run/ui-biz';
 import { Container } from 'inversify';
-import { CounterModel } from './counter.model';
 import { httpBatchLink, createTRPCClient, type TRPCClient } from '@trpc/client';
 import { AppRouter } from '@myshell-run/simple-services';
 import { MyAppTrpcClient } from '@myshell-run/def';
@@ -18,7 +17,6 @@ export const clientContainer = new Container();
 clientContainer.load(messageItemPluginsModule);
 clientContainer.load(uiBizModule);
 
-clientContainer.bind(CounterModel).toSelf().inSingletonScope();
 clientContainer
   .bind<TRPCClient<AppRouter>>(MyAppTrpcClient)
   .toConstantValue(trpc);
