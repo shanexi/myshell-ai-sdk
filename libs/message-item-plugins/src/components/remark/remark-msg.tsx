9e10ff19fb0d9b132dedbbe4e8f5a025a7c8e81f
shanexi <shane@myshell.ai>
2025-04-02 15:08:13 +0800

    feat: #85 remark stream update image (size diff, from small to raw)

--- libs/message-item-plugins/src/components/remark/remark-msg.tsx
+++ libs/message-item-plugins/src/components/remark/remark-msg.tsx
@@ -149,3 +149,32 @@ export const CounterStory = () => {
   console.log(text);
   return <RemarkMsg key="abc" user="me" text={text} />;
 };
+
+export const ImageStory = () => {
+  const lines = [
+    '\n::p[hello world]{.not-prose}',
+    `\n::img{#abc src=https://static.myshell.run/mini.PNG width=400}`,
+    `\n::img{#abc src=https://static.myshell.run/small.PNG width=400}`,
+    `\n::img{#abc src=https://static.myshell.run/medium.PNG width=400}`,
+    `\n::img{#abc src=https://static.myshell.run/big.PNG width=400}`,
+  ];
+  const [text, setText] = useState(lines[0]);
+  useEffect(() => {
+    let currentLine = 1;
+    const timer = setInterval(() => {
+      if (currentLine < lines.length) {
+        setText((prev: string) => {
+          const txt = prev + lines[currentLine];
+          currentLine++;
+          return txt;
+        });
+      } else {
+        clearInterval(timer);
+      }
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+  console.log(text);
+  return <RemarkMsg key="abc" user="me" text={text} />;
+};
