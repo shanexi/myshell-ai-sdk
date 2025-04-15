6cbdc9beebf0f5abf53ba6109e6571de4a275fb0
shanexi <shane@myshell.ai>
2025-04-15 13:05:57 +0800

    refactor: #100 增加一些 TODO 和注释

--- libs/biz/ui/src/components/chat-input-re.tsx
+++ libs/biz/ui/src/components/chat-input-re.tsx
@@ -20,13 +20,11 @@ export const ChatInput = observer<{ placeholder?: string }>(
   },
 );
 
-export const Wrapper: React.FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <Slot.Root className="border-border-default-light bg-surface-container-special-subtle-light">
-      <Slot.Slottable>{children}</Slot.Slottable>
-    </Slot.Root>
-  );
-};
+export const Wrapper: React.FC<PropsWithChildren> = ({ children }) => (
+  <Slot.Root className="border-border-default-light bg-surface-container-special-subtle-light">
+    <Slot.Slottable>{children}</Slot.Slottable>
+  </Slot.Root>
+);
 
 export const ChatInputRoot = observer<PropsWithChildren>(({ children }) => {
   const model = useInjection(ChatModel);
@@ -37,12 +35,13 @@ export const ChatInputRoot = observer<PropsWithChildren>(({ children }) => {
   });
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-  const timeout = 3000;
+  const timeout = 400;
   const [{ status, isMounted }, toggle] = useTransitionState({
     timeout,
     mountOnEnter: true,
     unmountOnExit: true,
     preEnter: true,
+    // TODO: 更细腻的 timeline 得用 GSAP 了
     // onStateChange: (ev) => {
     //   if (ev.current.isMounted) {
     //     model.setIsAnimatingInputFocus(true);
@@ -82,6 +81,7 @@ export const ChatInputRoot = observer<PropsWithChildren>(({ children }) => {
         createPortal(
           <div
             style={{
+              // 使用 inline style 是为了和 react transition-state 的 transitionDuration 保持一致
               transitionDuration: timeout + 'ms',
             }}
             className={cn(
@@ -100,6 +100,8 @@ export const ChatInputRoot = observer<PropsWithChildren>(({ children }) => {
             <Wrapper>
               <div
                 className={cn(
+                  // 26px 是 chat input + actions 的高度 52px 的一半，目的是 border radius 视觉上统一
+                  // TODO 去除 magic number 处理这种 直接用 style + const 好一点
                   'rounded-tl-4xl rounded-tr-4xl px-spacing-lg pb-[26px]',
                 )}
               >
