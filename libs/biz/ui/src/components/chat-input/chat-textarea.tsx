99cba9f599f6e9b6b74e0d6a91c22a9f197154d7
shanexi <shane@myshell.ai>
2025-04-15 13:12:52 +0800

    refactor: #100 将 chat text area 抽出来
    准备写上传区域

---
+++ libs/biz/ui/src/components/chat-input/chat-textarea.tsx
@@ -0,0 +1,99 @@
+import { cn, useInjection } from '@myshell-run/ui-primitives';
+import { reaction } from 'mobx';
+import { observer } from 'mobx-react-lite';
+import { useEffect, useRef } from 'react';
+import { createPortal } from 'react-dom';
+import TextareaAutosize from 'react-textarea-autosize';
+import { useTransitionState } from 'react-transition-state';
+import { ChatModel } from '../chat.model';
+import { Wrapper } from '../chat-input-re';
+
+export const ChatTextArea = observer(() => {
+  const model = useInjection(ChatModel);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const timeout = 400;
+  const [{ status, isMounted }, toggle] = useTransitionState({
+    timeout,
+    mountOnEnter: true,
+    unmountOnExit: true,
+    preEnter: true,
+    // TODO: 更细腻的 timeline 得用 GSAP 了
+    // onStateChange: (ev) => {
+    //   if (ev.current.isMounted) {
+    //     model.setIsAnimatingInputFocus(true);
+    //   } else {
+    //     model.setIsAnimatingInputFocus(false);
+    //   }
+    // },
+  });
+  // mobx 就不依赖 useEffect deps， 使用 mobx 自带的 reaction
+  useEffect(() => {
+    const disposer = reaction(
+      () => model.isInputFocus,
+      (isInputFocus) => {
+        if (isInputFocus) {
+          toggle(true);
+          setTimeout(() => {
+            textareaRef.current?.focus();
+          }); // 先让 input blur 再 textarea focus
+        } else {
+          toggle(false);
+        }
+      },
+    );
+    return () => disposer();
+  }, [toggle]);
+
+  return (
+    isMounted &&
+    createPortal(
+      <div
+        style={{
+          // 使用 inline style 是为了和 react transition-state 的 transitionDuration 保持一致
+          transitionDuration: timeout + 'ms',
+        }}
+        className={cn(
+          `fixed bottom-[26px] w-full px-spacing-md transition-all`,
+          {
+            'translate-y-full opacity-0':
+              status === 'preEnter' ||
+              status === 'unmounted' ||
+              status === 'exiting',
+
+            'translate-y-0 opacity-100':
+              status === 'entering' || status === 'entered',
+          },
+        )}
+      >
+        <Wrapper>
+          <div
+            className={cn(
+              // 26px 是 chat input + actions 的高度 52px 的一半，目的是 border radius 视觉上统一
+              // TODO 去除 magic number 处理这种 直接用 style + const 好一点
+              'rounded-tl-4xl rounded-tr-4xl px-spacing-lg pb-[26px]',
+            )}
+          >
+            <ChatInputHandlebar />
+            <TextareaAutosize
+              className="text-sm-regular w-full resize-none outline-none"
+              ref={textareaRef}
+              maxRows={8}
+              value={model.inputText}
+              onChange={(e) => model.setInputText(e.target.value)}
+            />
+          </div>
+        </Wrapper>
+      </div>,
+      document.body,
+    )
+  );
+});
+
+export const ChatInputHandlebar = () => {
+  return (
+    <div className="flex justify-center pt-spacing-md pb-spacing-lg">
+      <div className="w-[44px] rounded-full border-2 border-border-hovered-light"></div>
+    </div>
+  );
+};
