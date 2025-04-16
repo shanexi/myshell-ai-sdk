ee3b2a61df879acd581e8dc61d2b2c2a0c6eca22
shanexi <shane@myshell.ai>
2025-04-16 20:24:34 +0800

    refactor: #77 #85 more

--- libs/message-item-plugins/src/components/remark/remark-msg.stories.tsx
+++ libs/message-item-plugins/src/components/remark/remark-msg.stories.tsx
@@ -2,6 +2,7 @@ import { InversifyProvider, RemarkMsg } from '@myshell-run/ui-primitives';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Container } from 'inversify';
 import { messageItemPluginsModule } from '../../message-item-plugins.module';
+import remarkMsgStory from './remark-msg-story.md';
 
 const container = new Container();
 container.load(messageItemPluginsModule);
@@ -21,20 +22,7 @@ export default meta;
 export const Reply: StoryObj<typeof RemarkMsg> = {
   parameters: {},
   args: {
-    text: `:::main{#readme}
-
-Lorem:br
-ipsum.
-
-::hr
-
-A :i[lovely]{.text-red-500} language know as :abbr[HTML]{title="HyperText Markup Language"}.
-
-::button[🥰 generate]{#msg-id-generate.btn.btn-blue}
-
-:::
-::interactive-component
-`,
+    text: remarkMsgStory,
   },
 };
 
