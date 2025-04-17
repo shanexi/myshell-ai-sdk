8d72d8f2d210786ce59b1a83ad7b6101f4bf65fa
shanexi <shane@myshell.ai>
2025-04-17 10:10:56 +0800

    refactor: #104 more clean

--- libs/biz/ui/src/components/chat-input-re.tsx
+++ libs/biz/ui/src/components/chat-input-re.tsx
@@ -34,12 +34,10 @@ export const ChatInputRoot = observer<PropsWithChildren>(({ children }) => {
   return (
     <div ref={containerRef} className="absolute bottom-0 mb-2 w-full">
       <Wrapper>
-        <div className="relative z-20 mx-[8px] rounded-4xl">
+        <div className="relative z-10 mx-[8px] rounded-4xl">
           <div className="p-spacing-lg">{children}</div>
         </div>
       </Wrapper>
-      {/* 遮住 input 底部一半背景色，这样后面的层 translate Y 的时候会遮挡住底部 */}
-      {/* <div className="absolute bottom-0 z-10 h-[26px] w-full bg-surface-default-light"></div> */}
       <ChatTextArea />
     </div>
   );
