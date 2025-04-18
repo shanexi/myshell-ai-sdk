beb282ae54ddb63197bfcd8c2747b8b3bd6d2d2b
shanexi <shane@myshell.ai>
2025-04-18 10:36:55 +0800

    feat: #100 一些优化

--- libs/biz/ui/src/components/chat-input/chat-textarea.tsx
+++ libs/biz/ui/src/components/chat-input/chat-textarea.tsx
@@ -7,6 +7,10 @@ import { useTransitionState } from 'react-transition-state';
 import { Wrapper } from '../chat-input-re';
 import { ChatModel } from '../chat.model';
 
+// 这个 6px 是 parent element 比 children elements (目前只有textarea) 多出的 6px
+// 原因不明
+const UNKNOWN_6px = '6px';
+
 export const ChatTextArea = observer(() => {
   const model = useInjection(ChatModel);
   const textareaRef = useRef<HTMLTextAreaElement>(null);
@@ -25,9 +29,14 @@ export const ChatTextArea = observer(() => {
       (isInputFocus) => {
         if (isInputFocus) {
           toggle(true);
+          // `setTimeout` 先让 input blur 再 textarea focus
           setTimeout(() => {
             textareaRef.current?.focus();
-          }); // 先让 input blur 再 textarea focus
+            textareaRef.current?.setSelectionRange(
+              model.inputText.length,
+              model.inputText.length,
+            );
+          });
         } else {
           toggle(false);
         }
@@ -35,7 +44,6 @@ export const ChatTextArea = observer(() => {
     );
     return () => disposer();
   }, [toggle]);
-
   return (
     isMounted && (
       <div
@@ -51,7 +59,7 @@ export const ChatTextArea = observer(() => {
               status === 'preEnter' ||
               status === 'unmounted' ||
               status === 'exiting',
-            '-translate-y-[calc(100%-8px)] opacity-100':
+            [`-translate-y-[calc(100%-${UNKNOWN_6px})]`]:
               status === 'entering' || status === 'entered',
           },
         )}
@@ -59,10 +67,6 @@ export const ChatTextArea = observer(() => {
         <ChatInputHandlebar />
         <Wrapper>
           <TextareaAutosize
-            onHeightChange={(height, meta) => {
-              console.log('height', height);
-              console.log('meta', meta);
-            }}
             className="text-sm-regular w-full resize-none px-spacing-lg outline-none"
             ref={textareaRef}
             maxRows={8}
@@ -75,7 +79,9 @@ export const ChatTextArea = observer(() => {
   );
 });
 
-export const ChatInputHandlebar = () => {
+export const ChatInputHandlebar: React.FC<{
+  className?: string;
+}> = ({ className }) => {
   return (
     <Wrapper>
       <div
@@ -83,6 +89,7 @@ export const ChatInputHandlebar = () => {
           'flex justify-center rounded-tl-4xl rounded-tr-4xl pt-spacing-md pb-spacing-lg',
           'absolute top-0 -translate-y-full', // 永远在上方 像个屋顶
           'right-0 left-0 mx-2', // 针对 absolute 的元素，比 w-full 更好，w-full 会需要配合 box-border 总是搞不出来
+          className,
         )}
       >
         <div className="w-[44px] rounded-full border-2 border-border-hovered-light"></div>
