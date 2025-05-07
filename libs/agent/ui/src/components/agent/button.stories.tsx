f76c99d8d44e1d907cefd8cf73b5e4eb8b3924cd
shanexi <shane@myshell.ai>
2025-05-07 13:15:23 +0800

    chore: 验证能够 design system v2 v1 共存

---
+++ libs/agent/ui/src/components/agent/button.stories.tsx
@@ -0,0 +1,32 @@
+import { messageItemPluginsModule } from '@myshell-run/message-item-plugins';
+import { InversifyProvider } from '@myshell-run/ui-primitives';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Container } from 'inversify';
+import { agentUIModule } from '../../agent-ui.module';
+import { Button } from './button';
+
+const container = new Container();
+container.load(messageItemPluginsModule);
+container.load(agentUIModule);
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  decorators: [
+    (Story) => (
+      <InversifyProvider container={container}>
+        <Story />
+      </InversifyProvider>
+    ),
+  ],
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/design/YHfVjepmkzvSL4Yf1nD9KS/Shell-Agent?node-id=1408-77922&m=dev',
+    },
+  },
+};
+export default meta;
+
+export const Primary: StoryObj<typeof Button> = {
+  args: {},
+};
---
+++ libs/agent/ui/src/components/agent/button.tsx
@@ -0,0 +1,15 @@
+import { cn } from '@myshell-run/ui-primitives';
+
+export const Button = () => {
+  return (
+    <button
+      className={cn(
+        'bg-colors-background-normal-secondary-default-light-v2 dark:bg-colors-background-normal-secondary-default-dark-v2',
+        'text-colors-text-default-light-v2 dark:text-colors-text-default-dark-v2',
+        'p-spacing-lg-v2',
+      )}
+    >
+      Create
+    </button>
+  );
+};
