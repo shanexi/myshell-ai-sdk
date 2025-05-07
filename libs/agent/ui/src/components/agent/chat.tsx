b9303b36cb0c651098fe7d556f69430db8ae8e3c
shanexi <shane@myshell.ai>
2025-05-07 09:32:27 +0800

    chore: 验证 source none 之后，会按需生成

--- libs/agent/ui/src/components/agent/chat.tsx
+++ libs/agent/ui/src/components/agent/chat.tsx
@@ -3,7 +3,14 @@ import { cn } from '@myshell-run/ui-primitives';
 export const Chat = () => {
   return (
     <div className={cn('px-2')}>
-      <div className={cn('bg-surface-primary-default-light')}>Chat</div>
+      <div
+        className={cn(
+          'bg-surface-primary-default-light dark:bg-surface-primary-default-dark',
+          'text-text-default-light dark:text-text-default-dark',
+        )}
+      >
+        Chat
+      </div>
     </div>
   );
 };
