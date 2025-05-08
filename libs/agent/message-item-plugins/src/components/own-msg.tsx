669ba655d339dfea80e8f7cb1ff5fba14053a6aa
shanexi <shane@myshell.ai>
2025-05-08 14:28:17 +0800

    feat: agent own message

--- libs/agent/message-item-plugins/src/components/own-msg.tsx
+++ libs/agent/message-item-plugins/src/components/own-msg.tsx
@@ -1,9 +1,17 @@
 import { Message } from '@myshell-run/common-def';
+import { cn } from '@myshell-run/common-ui';
 
 export const OwnMessage = (props: Message) => {
   return (
-    <div className="flex pr-[8px] pb-8">
-      <div className="ml-auto max-w-[80%] rounded-tl-[16px] rounded-tr-[2px] rounded-br-[16px] rounded-bl-[16px] bg-surface-primary-subtle-default-light-v1 p-4 text-text-brand-light-v1">
+    <div className="flex px-spacing-xl-v2 py-spacing-lg-v2">
+      <div
+        className={cn(
+          'rounded-tl-components-bubble-radius-v2 rounded-tr-sm-v2 rounded-br-components-bubble-radius-v2 rounded-bl-components-bubble-radius-v2',
+          'ml-auto max-w-[80%] p-spacing-lg-v2',
+          'bg-colors-background-normal-secondary-default-light-v2 text-colors-text-default-light-v2',
+          'text-lg-regular',
+        )}
+      >
         {props.text}
       </div>
     </div>
