cfcd2ae78a90928677e419a921c167f02a0b30da
shanexi <shane@myshell.ai>
2025-05-08 10:25:55 +0800

    chore: agent chat input

--- libs/agent/ui/src/components/chat-input.tsx
+++ libs/agent/ui/src/components/chat-input.tsx
@@ -1,4 +1,6 @@
 import { cn } from '@myshell-run/ui-primitives';
+import TextareaAutosize from 'react-textarea-autosize';
+import { AtSign, CirclePlus, Mic } from 'lucide-react';
 
 export const ChatInput = () => {
   return (
@@ -7,10 +9,23 @@ export const ChatInput = () => {
         className={cn(
           'rounded-lg-v2 border border-component-colors-input-border-light-v2',
           'bg-component-colors-input-bg_default-light-v2',
-          'p-spacing-md-v2',
+          'p-spacing-xs-v2',
         )}
       >
-        Chat Input
+        <TextareaAutosize
+          className={cn(
+            'my-spacing-xs-v2 w-full resize-none px-spacing-sm-v2 outline-none',
+          )}
+          maxRows={8}
+          placeholder="Write a message"
+        />
+        <div className="flex items-center justify-between my-spacing-xs-v2">
+          <div className="flex items-center gap-[2px]">
+            <AtSign strokeWidth={1.5} size={36} className="p-[7px]" />
+            <CirclePlus strokeWidth={1.5} size={36} className="p-[7px]" />
+          </div>
+          <Mic strokeWidth={1.5} size={36} className="p-[7px]" />
+        </div>
       </div>
     </div>
   );
