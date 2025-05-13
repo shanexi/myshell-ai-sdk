1b874edfea8034a5c61b37ca59763eebf2578683
shanexi <shane@myshell.ai>
2025-05-13 15:18:19 +0800

    chore: add nx-plugin lib

---
+++ libs/nx-plugin/src/lib/nx-plugin.spec.ts
@@ -0,0 +1,7 @@
+import { nxPlugin } from './nx-plugin';
+
+describe('nxPlugin', () => {
+  it('should work', () => {
+    expect(nxPlugin()).toEqual('nx-plugin');
+  });
+});
---
+++ libs/nx-plugin/src/lib/nx-plugin.ts
@@ -0,0 +1,3 @@
+export function nxPlugin(): string {
+  return 'nx-plugin';
+}
