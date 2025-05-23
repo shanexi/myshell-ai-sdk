8ee2837382628a6241361df169babf59855b39cc
shanexi <shane@myshell.ai>
2025-05-23 10:14:47 +0800

    test fix

--- scripts/test-storybook.js
+++ scripts/test-storybook.js
@@ -26,6 +26,6 @@ run('biz-ui', 4600);
 run('biz-message-item-plugins', 4610);
 run('agent-ui', 4500);
 run('agent-message-item-plugins', 4510);
-run('agent-chat-input-plugis', 4520);
+run('agent-chat-input-plugins', 4520);
 
 console.log('\x1b[32m%s\x1b[0m', 'done');
