0af732246bf40d2ef771ace339ec8c322f53558b
shanexi <shane@myshell.ai>
2025-05-28 20:30:24 +0800

    fix: markdown

--- libs/agent/message-item-plugins/src/components/reply-msg.tsx
+++ libs/agent/message-item-plugins/src/components/reply-msg.tsx
@@ -4,7 +4,7 @@ import { RemarkMsg } from '@myshell-run/common-ui';
 export const ReplyMsg: React.FC<Message> = (props) => {
   const { text } = props;
   return (
-    <div className="px-spacing-xl-v2 py-spacing-lg-v2">
+    <div className="prose dark:prose-invert px-spacing-xl-v2 py-spacing-lg-v2">
       <RemarkMsg text={text} />
     </div>
   );
