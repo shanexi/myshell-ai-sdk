2efed7ab979216ad59811d35f4cb17ef69cf0fa1
shanexi <shane@myshell.ai>
2025-06-04 13:00:35 +0800

    refactor: #156 将 edix 相关逻辑重构到 model

--- libs/agent/chat-input-plugins/src/components/chat-input-advanced-input-plugin.tsx
+++ libs/agent/chat-input-plugins/src/components/chat-input-advanced-input-plugin.tsx
@@ -1,5 +1,4 @@
 import { cn } from '@myshell-run/common-ui';
-import { Delete, editable, EditableHandle, plainSchema } from 'edix';
 import { useInjection } from 'inversify-react';
 import { observer } from 'mobx-react-lite';
 import { isEmpty } from 'radash';
@@ -10,13 +9,9 @@ export const ChatInputAdvancedInputPlugin = observer(() => {
   const ref = useRef<HTMLDivElement>(null);
   const model = useInjection(ChatInputModel);
 
-  const handle = useRef<EditableHandle | null>(null);
   useEffect(() => {
     if (!ref.current) return;
-    return (handle.current = editable(ref.current, {
-      schema: plainSchema({ multiline: true }),
-      onChange: model.chatCommon.setInputText,
-    })).dispose;
+    return model.setEdixRef(ref);
   }, []);
 
   const value = model.chatCommon.inputText;
@@ -36,12 +31,6 @@ export const ChatInputAdvancedInputPlugin = observer(() => {
             e.preventDefault();
             if (!ref.current) return;
             model.sendText();
-            // todo: 这种方式会有选中，而且不会清空 history，如果不满足需求，需要进一步 patch
-            // 核心是不能通过外部 setValue(更新) value，而应该是 edix -> setValue -> value(render) 这样，所有 modification 都必须从 edix
-            ref.current.focus();
-            window.getSelection()?.selectAllChildren(ref.current);
-            handle.current?.syncSelection();
-            setTimeout(() => handle.current?.command(Delete));
           }
         }}
         aria-placeholder="Write a message"
--- libs/agent/chat-input-plugins/src/components/chat-input.model.ts
+++ libs/agent/chat-input-plugins/src/components/chat-input.model.ts
@@ -3,7 +3,9 @@ import { ChatCommonModel, MOCK_IMG } from '@myshell-run/common-ui';
 import { inject, injectable } from 'inversify';
 import { computed, makeObservable, observable } from 'mobx';
 import { isEmpty } from 'radash';
+import { RefObject } from 'react';
 import { z } from 'zod';
+import { Delete, editable, EditableHandle, plainSchema } from 'edix';
 
 export const ChatInputHandlers = Symbol.for('ChatInputHandlers');
 export type ContextType = 'file' | 'text' | 'json' | 'todo' | 'message';
@@ -57,9 +59,10 @@ export class ChatInputModel {
     },
   ]);
 
-  // private ref?: RefObject<HTMLDivElement>;
-  // private refPromise: Promise<boolean>;
-  // private refResolve?: (value: boolean | PromiseLike<boolean>) => void;
+  private edixRef?: RefObject<HTMLDivElement>;
+  private edixHandle: EditableHandle | null = null;
+  public edixRefPromise: Promise<boolean>;
+  private edixRefResolve?: (value: boolean | PromiseLike<boolean>) => void;
 
   constructor(
     @inject(ChatInputHandlers) private handlers: ChatInputHandlers,
@@ -68,17 +71,37 @@ export class ChatInputModel {
   ) {
     makeObservable(this);
 
-    // this.refPromise = new Promise<boolean>((resolve) => {
-    //   this.refResolve = resolve;
-    // });
+    this.edixRefPromise = new Promise<boolean>((resolve) => {
+      this.edixRefResolve = resolve;
+    });
   }
 
-  // setRef(ref: RefObject<HTMLDivElement>) {
-  //   this.ref = ref;
-  //   if (this.refResolve) {
-  //     this.refResolve(true);
-  //   }
-  // }
+  setEdixRef(ref: RefObject<HTMLDivElement>) {
+    if (!ref.current) return;
+
+    this.edixRef = ref;
+    if (this.edixRefResolve) {
+      this.edixRefResolve(true);
+    }
+
+    const dispose = (this.edixHandle = editable(ref.current, {
+      schema: plainSchema({ multiline: true }),
+      onChange: this.chatCommon.setInputText,
+    })).dispose;
+
+    return () => {
+      this.resetEdixRef();
+      dispose();
+    };
+  }
+
+  resetEdixRef() {
+    this.edixRef = undefined;
+    this.edixHandle = null;
+    this.edixRefPromise = new Promise<boolean>((resolve) => {
+      this.edixRefResolve = resolve;
+    });
+  }
 
   @computed get isContextItemsEmpty() {
     return this.contextItems.length === 0;
@@ -101,6 +124,7 @@ export class ChatInputModel {
       // TODO 不能，全部交给 edix#onChange 管理了
       // 应该封装下，不让外部操作
       // this.chatCommon.setInputText('');
+      await this.clearEdix();
     }
   }
 
@@ -130,6 +154,22 @@ export class ChatInputModel {
   removeImagePreview(id: string) {
     for (const _ of this.handlers.removeImagePreview(id)) {
       // 其他操作
+    }
+  }
+
+  async clearEdix() {
+    await this.edixRefPromise;
+    // todo: 这种方式会有选中，而且不会清空 history，如果不满足需求，需要进一步 patch
+    // 核心是不能通过外部 setValue(更新) value，而应该是 edix -> setValue -> value(render) 这样，所有 modification 都必须从 edix
+    if (this.edixRef?.current && this.edixHandle) {
+      this.edixRef?.current.focus();
+      window.getSelection()?.selectAllChildren(this.edixRef.current);
+      this.edixHandle.syncSelection();
+      setTimeout(() => {
+        if (this.edixHandle) {
+          this.edixHandle.command(Delete);
+        }
+      });
     }
   }
 }
