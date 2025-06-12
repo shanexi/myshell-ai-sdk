8f8e1be55328d35d74fee02e8902c080fc65032f
shanexi <shane@myshell.ai>
2025-06-12 17:13:13 +0800

    #2 ci 禁用 test-storybook，准备用 chromatic

--- libs/preview-chat/chat-input-plugins/src/components/chat-input-advanced-input-plugin.stories.tsx
+++ libs/preview-chat/chat-input-plugins/src/components/chat-input-advanced-input-plugin.stories.tsx
@@ -48,39 +48,39 @@ export const Primary: StoryObj<typeof ChatInputAdvancedInputPlugin> = {
   args: {},
   // 似乎 cypress 不能 test 这个
   // TODO: 后面迁移到 chromatic
-  // play: async ({ canvasElement }) => {
-  //   const canvas = within(canvasElement);
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
 
-  //   // Find the contenteditable div by CSS selector
-  //   const input = canvasElement.querySelector(
-  //     '[contenteditable="true"]',
-  //   ) as HTMLElement;
+    // Find the contenteditable div by CSS selector
+    const input = canvasElement.querySelector(
+      '[contenteditable="true"]',
+    ) as HTMLElement;
 
-  //   // 1. Input "abc"
-  //   await userEvent.click(input);
-  //   await userEvent.type(input, 'abc');
+    // 1. Input "abc"
+    await userEvent.click(input);
+    await userEvent.type(input, 'abc');
 
-  //   await new Promise((r) => setTimeout(r, 1000));
+    await new Promise((r) => setTimeout(r, 1000));
 
-  //   // TODO: 是成功了，但是光标没有到下一行，先不测试这个 case
-  //   // 2. Shift + Enter for new line
-  //   // await userEvent.keyboard('{Shift>}{Enter}{/Shift}');
+    // TODO: 是成功了，但是光标没有到下一行，先不测试这个 case
+    // 2. Shift + Enter for new line
+    // await userEvent.keyboard('{Shift>}{Enter}{/Shift}');
 
-  //   // 3. Input "def"
-  //   await userEvent.type(input, 'def');
+    // 3. Input "def"
+    await userEvent.type(input, 'def');
 
-  //   await new Promise((r) => setTimeout(r, 1000));
+    await new Promise((r) => setTimeout(r, 1000));
 
-  //   // 4. Press Enter to send
-  //   await userEvent.keyboard('{Enter}');
+    // 4. Press Enter to send
+    await userEvent.keyboard('{Enter}');
 
-  //   await new Promise((r) => setTimeout(r, 1000));
+    await new Promise((r) => setTimeout(r, 1000));
 
-  //   // 5. Command + Z to undo
-  //   await userEvent.keyboard('{Meta>}z{/Meta}');
-  //   await new Promise((r) => setTimeout(r, 1000));
+    // 5. Command + Z to undo
+    await userEvent.keyboard('{Meta>}z{/Meta}');
+    await new Promise((r) => setTimeout(r, 1000));
 
-  //   // 6. Verify text content
-  //   expect(input.textContent).toBe('abcdef');
-  // },
+    // 6. Verify text content
+    expect(input.textContent).toBe('abcdef');
+  },
 };
